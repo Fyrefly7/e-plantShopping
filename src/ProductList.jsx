a9d0dad37ca2e2e0a1a67b519934f1226076168c
Fyrefly7 <ejpingolt@gmail.com>
2025-02-21 17:50:38 -0500

    handleAddToCart added

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,11 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-    const [addedToCart, setAddedToCart] = useState([]);
+    const [addedToCart, setAddedToCart] = useState({});
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -245,6 +246,13 @@ const handlePlantsClick = (e) => {
    const handleContinueShopping = (e) => {
     e.preventDefault();
     setShowCart(false);
+  };
+
+  const handleAddToCart = (plant) => {
+    dispatch(addItem(plant));
+    setAddedToCart((prevState) => (
+        {...prevState, [product.name]: true}
+    ));
   };
     return (
         <div>

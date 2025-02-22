2237ca14f50c01764b0e11573a491db3ca46aff3
Fyrefly7 <ejpingolt@gmail.com>
2025-02-22 14:38:14 -0500

    finish CartItem functions

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -25,18 +25,24 @@ const handleCheckoutShopping = (e) => {
 };
 
   const handleIncrement = (item) => {
-    dispatch(updateQuantity({item.name, item.quantity+1}));
+    dispatch(updateQuantity({name:item.name, quantity:item.quantity+1}));
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity > 1) {
+        dispatch(updateQuantity({name:item.name, quantity:item.quantity+1}));
+    } else {
+        dispatch(removeItem(item.name));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.quantity * parseFloat(item.cost.substring(1));
   };
 
   return (
